adbdd7d8e79a557648fe2f491039122c7cbd6fa9
John Bellone <john.bellone.jr@gmail.com>
2014-06-02 22:37:26 -0400

    Fix tag for chef-search.

--- libraries/util.rb
+++ libraries/util.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: blp-bcpc
 # Library:: util
 #
-# Copyright (C) 2013-2014 Bloomberg Finance L.P.
+# Copyright (C) 2013, 2014 Bloomberg Finance L.P.
 #
 def get_all_nodes
   nodes = search(:node, "chef_environment=#{node.chef_environment} NOT name:#{chef.name}")
@@ -11,13 +11,13 @@ def get_all_nodes
 end
 
 def get_head_nodes
-  nodes = search(:node, "tag:bcpc-headnode AND chef_environment=#{node.chef_environment} NOT name:#{chef.name}")
+  nodes = search(:node, "tag:bcpc.headnode AND chef_environment=#{node.chef_environment} NOT name:#{chef.name}")
   nodes << node
   nodes
 end
 
 def get_work_nodes
-  nodes = search(:node, "tag:bcpc-worknode AND chef_environment=#{node.chef_environment} NOT name:#{chef.name}")
+  nodes = search(:node, "tag:bcpc.worknode AND chef_environment=#{node.chef_environment} NOT name:#{chef.name}")
   nodes << node
   nodes
 end
