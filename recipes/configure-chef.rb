e257bcb0b966c1f06539a211cc2e10fbd512076d
John Bellone <john.bellone.jr@gmail.com>
2014-06-03 06:03:30 -0400

    Fix chef-client recipe inclusion.
    This fixes a few issues with the Chef client recipes, namely the
    mistyping of the delete_validation recipe. But it also adds the default
    recipe to configure the chef-client as a service.

--- recipes/configure-chef.rb
+++ recipes/configure-chef.rb
@@ -10,6 +10,7 @@ include_recipe 'chef-sugar::default'
 if tagged?('bcpc.headnode')
   include_recipe 'chef-server::default'
 else
-  include_recipe 'chef-client::delete-validation'
   include_recipe 'chef-client::config'
+  include_recipe 'chef-client::default'
+  include_recipe 'chef-client::delete_validation'
 end
