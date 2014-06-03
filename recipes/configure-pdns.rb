d1b21af22eb30379e0aa79acd1592605efbfa33c
John Bellone <john.bellone.jr@gmail.com>
2014-06-03 06:19:19 -0400

    Add pdns server recipe.

---
+++ recipes/configure-pdns.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: blp-bcpc
+# Recipe:: configure-apache
+#
+# Copyright (C) 2013, 2014 Bloomberg Finance L.P.
+#
+
+include_recipe 'chef-sugar::default'
+
+include_recipe 'pdns::server'
--- recipes/headnode.rb
+++ recipes/headnode.rb
@@ -8,3 +8,4 @@
 node.tag('bcpc.headnode')
 
 include_recipe 'blp-bcpc::worknode'
+include_recipe 'blp-bcpc::configure-pdns'
