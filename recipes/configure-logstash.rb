74686beaae51d1ae82117c26c9aff7d876b51316
John Bellone <john.bellone.jr@gmail.com>
2014-06-03 06:22:23 -0400

    Setup logstash using community cookbook.

--- recipes/configure-logstash.rb
+++ recipes/configure-logstash.rb
@@ -4,3 +4,16 @@
 #
 # Copyright (C) 2013, 2014 Bloomberg Finance L.P.
 #
+
+include_recipe 'chef-sugar::default'
+
+node.default['logstash']['elasticsearch_query'] = 'tag:bcpc.headnode'
+node.default['logstash']['graphite_query'] = 'tag:bcpc.headnode'
+
+if tagged?('bcpc.headnode')
+  node.default['logstash']['install_rabbitmq'] = false
+
+  include_recipe 'logstash::server'
+else
+  include_recipe 'logstash::beaver'
+end
